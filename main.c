d57f69a28cf32460138eb7922ac9ea6bb95276ea
hedi-ben-maouiia <teedytech@gmail.com>
2024-01-20 12:17:57 +0100

    new update

--- main.c
+++ main.c
@@ -3,8 +3,14 @@
 int foo(int x, int y){
    return x + y; 
 }
+int fac(int n){
+    if(n == 1)
+        return 1;
+    return n * fac(n-1);
+}
 int main(void){
     foo(10,2);
+    fac(2);
     printf("Hello, World!\n"); 
     return 0;
 }
